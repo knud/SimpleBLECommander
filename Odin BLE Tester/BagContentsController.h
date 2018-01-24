bc08fb5562671aa8908ba1af998acc59053e0cda
Steven Knudsen <ksk@ieee.org>
2018-01-24 10:48:52 -0700

    New assets and re-architect the BLE command and response

---
+++ Odin BLE Tester/BagContentsController.h
@@ -0,0 +1,13 @@
+//
+//  BagContentsController.h
+//  Odin BLE Tester
+//
+//  Created by Knud S Knudsen on 2018-01-21.
+//  Copyright © 2018 TechConficio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BagContentsController : UITableViewController
+
+@end
